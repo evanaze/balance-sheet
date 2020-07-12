42e93b9fd0b9701d5f5cd519a67293bb4026dd6a
Evan Azevedo <evan@azevedo.com>
2020-07-12 15:50:56 -0700

    working balancesheet

--- src/balanceSheet.py
+++ src/balanceSheet.py
@@ -29,25 +29,36 @@ class BalanceSheet:
         self.con.commit()
 
     def read(self):
+        "Reads the current balance sheet"
         cursorObj = self.con.cursor()
         cursorObj.execute("SELECT type, name, value, description FROM balancesheet")
         rows = cursorObj.fetchall()
         self.data = pd.DataFrame(rows, columns = ["Type", "Name", "Value", "Description"])
         self.data = self.data.set_index("Type").sort_index()
 
+    def get_date(self):
+        "Gets the most recent balance sheet"
+        cursorObj = self.con.cursor()
+        cursorObj = cursorObj.execute("SELECT date FROM lastupdated ORDER BY table_id LIMIT 1")
+        print("Date:", cursorObj.readone())
+
     def insert(self, x):
+        "inserts an asset or liability"
         cursorObj = self.con.cursor()
         cursorObj.execute('INSERT INTO balancesheet(table_id, type, name, value, description) VALUES(?, ?, ?, ?, ?)', x)
         self.con.commit()
 
     def display(self):
         "Displays the current state of the balance sheet"
+        # assets
         ass = self.data.loc['Asset']
         print(f"\nAssets:\n{ass}")
         tot_ass = ass.Value.sum()
         print(f"Total Assets: {tot_ass}")
+        # liabilities
         liab = self.data.loc['Liability']
         print(f"\nLiabilities:\n{liab}")
         tot_liab = liab.Value.sum()
+        # net worth
         print(f"Total Liabilities: {tot_liab}")
         print(f"Net worth: {tot_ass - tot_liab}")
