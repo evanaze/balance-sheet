d1c460dfb57bfc35b1f5d3c68e2e1cbf26edf01c
Evan Azevedo <evan@azevedo.com>
2020-08-01 15:46:00 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="balance-sheet",
-    version="0.1.0",
+    version="0.2.0",
     description="A personal balance sheet program",
     long_description=README,
     long_description_content_type="text/markdown",
